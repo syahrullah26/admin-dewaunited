911a91ac05cecf5f09141f8ed3c001787bed6c73
Muhammad Syahrulah <muhammadsyahrullah26@gmail.com>
2026-01-27 14:37:23 +0700

    minor fix env.d.ts

--- env.d.ts
+++ env.d.ts
@@ -1,7 +1,17 @@
-/// <reference types="vite/client">
+/// <reference types="vite/client" />
 
 declare module '*.vue' {
-    import type { DefineComponent } from "vue"
-    const component: DefineComponent<{}, {}, any>
-    export default component
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string
+  // contoh kalau perlu:
+  // readonly VITE_APP_NAME: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
 }
